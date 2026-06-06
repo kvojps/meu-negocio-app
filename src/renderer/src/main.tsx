b78e2e52c8379ec39339607e5800f453f34f5fa4
Kvojps <josebazjunior09@gmail.com>
2026-06-05 22:53:51 -0300

    [refac]: add @fontsource/inter for font management and remove Google Fonts import

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/700.css';
 import './theme.css';
 import './styles.css';
 
