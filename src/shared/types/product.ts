9dc0bc22e118ce0164ad133c04239518482740c3
Kvojps <josebazjunior09@gmail.com>
2026-06-11 19:54:21 -0300

    [refac]: add Product interface and mock product data for testing

---
+++ src/shared/types/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  supplier: string;
+  costPrice: number;
+  salePrice: number;
+  stock: number;
+  minStock: number;
+  createdAt: string;
+  updatedAt: string;
+}
